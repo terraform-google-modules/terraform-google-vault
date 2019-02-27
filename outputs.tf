70fcb0c01c0d94310fe365feea430a7f55c547ec
Seth Vargo <seth@sethvargo.com>
2019-02-27 17:40:54 -0500

    Document all outputs

--- outputs.tf
+++ outputs.tf
@@ -1,35 +1,63 @@
-/*
- * Copyright 2017 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-output instance_group {
-  description = "Link to the `instance_group` property of the instance group manager resource."
-  value       = "${module.vault-server.instance_group}"
-}
-
-output ca_private_key_algorithm {
-  description = "The root CA algorithm for generating client certs."
-  value       = "${tls_private_key.root.algorithm}"
-}
-
-output ca_private_key_pem {
-  description = "The root CA key pem for generating client certs."
-  value       = "${tls_private_key.root.private_key_pem}"
-}
+#
+# Copyright 2017 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 output ca_cert_pem {
-  description = "The root CA cert pem for generating client certs."
-  value       = "${tls_self_signed_cert.root.cert_pem}"
+  value     = "${tls_self_signed_cert.root.cert_pem}"
+  sensitive = true
+
+  description = <<EOF
+CA certificate used to verify Vault TLS client connections.
+EOF
+}
+
+output ca_key_pem {
+  value     = "${tls_private_key.root.private_key_pem}"
+  sensitive = true
+
+  description = <<EOF
+Private key for the CA.
+EOF
+}
+
+output vault_addr {
+  value = "https://${google_compute_address.vault.address}:${var.vault_port}"
+
+  description = <<EOF
+Full protocol, address, and port (FQDN) pointing to the Vault load balancer.
+This is a drop-in to VAULT_ADDR:
+
+    export VAULT_ADDR="$(terraform output vault_addr)"
+
+And then continue to use Vault commands as usual.
+EOF
+}
+
+output vault_lb_addr {
+  value = "${google_compute_address.vault.address}"
+
+  description = <<EOF
+Address of the load balancer without port or protocol information. You probably
+want to use `vault_addr`.
+EOF
+}
+
+output vault_lb_port {
+  value = "${var.vault_port}"
+
+  description = <<EOF
+Port where Vault is exposed on the load balancer.
+EOF
 }
