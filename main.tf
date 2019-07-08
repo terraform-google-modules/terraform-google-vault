83458f4acbc26981b5a37a3fe8f3319d8f7935aa
Daniel Caballero <dcaballero@fastly.com>
2019-07-08 10:14:28 +0200

    extracting storage props to its own tf file

--- main.tf
+++ main.tf
@@ -15,8 +15,6 @@
 #
 
 locals {
-  # Allow the user to specify a custom bucket name, default to project-id prefix
-  storage_bucket_name = "${var.storage_bucket_name != "" ? var.storage_bucket_name : "${var.project_id}-vault-data"}"
   vault_tls_bucket    = "${var.vault_tls_bucket != "" ? var.vault_tls_bucket : local.storage_bucket_name}"
 
   # Inverts logic from user perspective to terraform perspective for use in count
@@ -42,23 +40,6 @@ resource "google_project_service" "service" {
   disable_on_destroy = false
 }
 
-# Create the storage bucket for where Vault data will be stored. This is a
-# multi-regional storage bucket so it has the highest level of availability.
-resource "google_storage_bucket" "vault" {
-  project = "${var.project_id}"
-
-  name          = "${local.storage_bucket_name}"
-  location      = "${upper(var.storage_bucket_location)}"
-  storage_class = "MULTI_REGIONAL"
-
-  versioning {
-    enabled = "${var.storage_versioning}"
-  }
-
-  force_destroy = "${var.storage_bucket_force_destroy}"
-
-  depends_on = ["google_project_service.service"]
-}
 
 # Create the vault-admin service account.
 resource "google_service_account" "vault-admin" {
---
+++ storage.tf
@@ -0,0 +1,22 @@
+locals {
+  # Allow the user to specify a custom bucket name, default to project-id prefix
+  storage_bucket_name = "${var.storage_bucket_name != "" ? var.storage_bucket_name : "${var.project_id}-vault-data"}"
+}
+
+# Create the storage bucket for where Vault data will be stored. This is a
+# multi-regional storage bucket so it has the highest level of availability.
+resource "google_storage_bucket" "vault" {
+  project = "${var.project_id}"
+
+  name          = "${local.storage_bucket_name}"
+  location      = "${upper(var.storage_bucket_location)}"
+  storage_class = "MULTI_REGIONAL"
+
+  versioning {
+    enabled = "${var.storage_versioning}"
+  }
+
+  force_destroy = "${var.storage_bucket_force_destroy}"
+
+  depends_on = ["google_project_service.service"]
+}
