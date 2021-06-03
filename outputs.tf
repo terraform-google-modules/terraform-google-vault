2ec2e076c7534ba6c52869ceb550c66e321fcd80
Abdulla Mulail Mohamed <mulailmohamed@gmail.com>
2021-06-03 21:49:53 +0800

    feat(vault): remove NAT resources
    Signed-off-by: Abdulla Mulail Mohamed <mulailmohamed@gmail.com>

--- outputs.tf
+++ outputs.tf
@@ -66,8 +66,3 @@ output "vault_subnet" {
   value       = local.subnet
   description = "The subnetwork in which the Vault cluster resides"
 }
-
-output "vault_nat_ips" {
-  value       = google_compute_address.vault-nat.*.address
-  description = "The NAT-ips that the vault nodes will use to communicate with external services."
-}
